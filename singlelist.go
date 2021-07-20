a14dee2808c92aeea9edb65f260afc0c0c14de9a
KScorpion <thelastsun@163.com>
2021-07-20 16:50:59 +0800

    单链表-头插法-尾插法-链表反转
    单链表-头插法-尾插法-链表反转

--- singlelist.go
+++ singlelist.go
@@ -2,68 +2,60 @@ package main
 
 import "fmt"
 
-type List struct {
+type L1 struct {
 	data int
-	next *List
+	next *L1
 }
 
-var (
-	link *List
-	p *List
-	node *List
-	newHead *List
-	newNode *List
-	recv *List
-	n int
-)
-
-func InitNode ()(link *List){
-	link = &List{}
-	return
+//头插法
+func createListHead(n int) *L1 {
+	temp := &L1{}
+	temp = nil
+	for i := n; i >= 0; i-- {
+		p := &L1{}
+		p.data = i
+		p.next = temp
+		temp = p
+	}
+	return temp
 }
 
-func BuildLink()(node *List){
-	link = InitNode()
-	for n=1;n<10;n++{
-		node = InitNode()
-		node.data = n
-		node.next = link
-		link = node
+//尾插法
+func createListTail(n int) *L1 {
+	H := &L1{}
+	temp := H
+	for i := n; i >= 0; i-- {
+		p := &L1{}
+		p.data = i
+		temp.next = p
+		temp = p
+	}
+	temp.next = nil
+	return H.next
+}
+
+func LinkRec(p *L1) (newHead *L1) {
+	newNode := &L1{}
+	for p != nil {
+		newNode = p
+		p = p.next
+		newNode.next = newHead
+		newHead = newNode
 	}
 	return
 }
 
-func LinkRecv(p *List)(newHead *List){
-	newHead = nil
-	newNode = InitNode()
-	for  {
-		if p!=nil {
-			newNode = p
-			p = p.next
-			newNode.next = newHead
-			newHead = newNode
-		}else {
-			break
-		}
+func displayL1(l1 *L1) {
+	for l1 != nil {
+		fmt.Println(l1.data)
+		l1 = l1.next
 	}
-	return
 }
-
-func PrintLink(node *List){
-	for{
-		if node!=nil {
-			fmt.Printf("%d",node.data)
-			node = node.next
-		}else {
-			break
-		}
-	}
-	println()
-}
-
-func main(){
-	p = BuildLink()
-	PrintLink(p)
-	recv = LinkRecv(p)
-	PrintLink(recv)
+func main() {
+	fmt.Println("头插结果")
+	displayL1(createListHead(10))
+	fmt.Println("尾插结果")
+	displayL1(createListTail(10))
+	fmt.Println("反转结果")
+	displayL1(LinkRec(createListTail(10)))
 }
