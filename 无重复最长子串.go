7abe78d62725d97116aa37baf352b62f10fc7ec0
KScorpion <thelastsun@163.com>
2022-01-13 13:36:28 +0800

    Update 无重复最长子串.go

--- 无重复最长子串.go
+++ 无重复最长子串.go
@@ -14,7 +14,7 @@ func lengthOfLongestSubString(s string) int {
 	left := 0
 	for right := 0; right < length; right++ {
 		index := s[right]
-		//单个字符
+		//若有重复字符 左指针移动到该位置
 		//两种情况 1存在过 【字符下标】 2没存在过 【0】
 		left = max(left, cmap[index])
 		ans = max(ans, right-left+1)
