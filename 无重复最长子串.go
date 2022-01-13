9585af8b68a551586cf05488e6417cacb741912c
KScorpion <thelastsun@163.com>
2022-01-13 10:31:17 +0800

    Create 无重复最长子串.go

---
+++ 无重复最长子串.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "abba"
+	fmt.Println(lengthOfLongestSubString(s))
+}
+
+func lengthOfLongestSubString(s string) int {
+	length := len(s)
+	ans := 0
+	cmap := make([]int, 128)
+	left := 0
+	for right := 0; right < length; right++ {
+		index := s[right]
+		//单个字符
+		//两种情况 1存在过 【字符数值】 2没存在过 【0】
+		left = max(left, cmap[index])
+		ans = max(ans, right-left+1)
+		cmap[index] = right + 1
+	}
+	return ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
