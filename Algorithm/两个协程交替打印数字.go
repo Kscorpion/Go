21ce95a72797123633215bc0c3f8761d1019a6f7
kscorpion <thelastsun@163.com>
2022-04-03 20:47:10 +0800

    增加go两道工程题

---
+++ Algorithm/两个协程交替打印数字.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan int)
+	ctx, cancelFunc := context.WithTimeout(context.TODO(), 5*time.Second)
+	go func(ctx context.Context, cancelFunc1 context.CancelFunc, ch chan int) {
+		for i := 1; i <= 100; i += 1 {
+			<-ch
+			if i%2 == 0 {
+				fmt.Println(i)
+				if i == 100 {
+					close(ch)
+					cancelFunc1()
+				}
+			}
+		}
+	}(ctx, cancelFunc, ch)
+
+	go func(ctx context.Context) {
+		for i := 1; i <= 100; i += 1 {
+			ch <- 0
+			if i%2 == 1 {
+				fmt.Println(i)
+			}
+		}
+	}(ctx)
+	select {
+	case <-ctx.Done():
+		cancelFunc()
+		fmt.Println("结束输出")
+	}
+}
---
+++ Algorithm/超时控制.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+var ()
+
+func main() {
+	c := make(chan int, 10)
+	ctx, cancelfunc := context.WithTimeout(context.TODO(), 5*time.Second)
+	list := []int{23, 32, 78, 43, 76, 65, 345, 762}
+	go a(ctx, list, 345, c)
+	go a(ctx, list, 345, c)
+	select {
+	case <-c:
+		cancelfunc()
+		return
+	case <-ctx.Done():
+		fmt.Println("超时了")
+		cancelfunc()
+		return
+	}
+}
+
+func a(cancel context.Context, list []int, flg int, c chan<- int) {
+	for _, val := range list {
+		if val == flg {
+			//time.Sleep(10 * time.Second)
+			fmt.Println("Found it")
+			c <- 10
+			close(c)
+			break
+		}
+	}
+}
