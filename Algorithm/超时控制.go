8a6a1adda62b1e3ea6a64281dda9e562cd121390
KScorpion <thelastsun@163.com>
2024-03-26 20:17:27 +0800

    Update 超时控制.go 更新

--- Algorithm/超时控制.go
+++ Algorithm/超时控制.go
@@ -7,44 +7,31 @@ import (
 )
 
 type API interface {
-	Call(time.Duration) (string, error)
+	Call(context.Context, time.Duration) (string, error)
 }
 
 type MyAPI struct {
 }
 
-func (a *MyAPI) Call(t time.Duration) (string, error) {
-	time.Sleep(t * time.Second)
-	return "success call", nil
+func (a *MyAPI) Call(ctx context.Context, id string) {
+	select {
+	case <-ctx.Done():
+		fmt.Println("这个协程退出了奥", id) // 返回上下文错误，表示取消原因
+	}
 }
 
 func main() {
-	ctx, calcel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer calcel()
+	ctx1, cancel1 := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel1()
+	ctx2, cancel2 := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel2()
+	ctx3, cancel3 := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel3()
 	api := &MyAPI{}
-	result := make(chan string)
 
-	go func() {
-		data, _ := api.Call(1)
-		result <- data
-	}()
+	go api.Call(ctx1, "1")
+	go api.Call(ctx2, "2")
+	go api.Call(ctx3, "3")
 
-	go func() {
-		data, _ := api.Call(3)
-		result <- data
-	}()
-
-	go func() {
-		data, _ := api.Call(5)
-		result <- data
-	}()
-
-	for i := 0; i < 3; i++ {
-		select {
-		case <-ctx.Done():
-			fmt.Println("timeout")
-		case res := <-result:
-			fmt.Println(res)
-		}
-	}
+	time.Sleep(10 * time.Second)
 }
